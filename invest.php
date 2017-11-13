eb42994a0d8f1d84ad52fddabb343b1dfa62aa3d
maximombro <maximombro@gmail.com>
2017-11-13 15:00:06 -0500

    Toolbar given a selector indicator, More spooky skeletons

---
+++ invest.php
@@ -0,0 +1,19 @@
+<!-- Header Loader -->
+<?php
+    include ("assets/completeTop.php");
+?>
+
+<!-- Background Panel Start -->
+<div class="backgroundPanel">
+
+<!-- Page Content -->
+<h3>Invest Page</h3>
+<p>Test text for getting stuff to work right before putting in effort.</p>
+
+<!-- Background Panel End -->
+</div>
+
+<!-- Footer Loader -->
+<?php 
+    include ("assets/footer.php"); 
+?>
