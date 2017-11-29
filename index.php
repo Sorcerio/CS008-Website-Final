8aa1efeb3fc5090268debe46f5159fc069141fc1
maximombro <maximombro@gmail.com>
2017-11-29 14:19:06 -0500

    Added text to the panels on the Homepage

--- index.php
+++ index.php
@@ -12,20 +12,20 @@
     </div>
     
     <div class="backgroundPanel homePageFlexBox">
-        <h1 class="bpHeader">Join Us</h1>
-        <p>Some text describing our form and what we use it for, etc. Need ~85 words.</p>
+        <h1 class="bpHeader">News Letter</h1>
+        <p>If you're a fan of Clean Burning Black Rocks Inc. and are burning to get a feel for more of our Clean Burning Black Rocks, be sure to sign up for our News Letter on the Join Page. All you need to do is fill out a simple form where we collect your email as well as any donations you may want to send to us; hint, hint, nudge, nudge. Any choice of donation level will help us greatly in our future endeavors and general research. Just click the link below to get started!</p>
         <a href="join.php" class="bpButton">More Information</a>
     </div>
 
     <div class="backgroundPanel homePageFlexBox">
         <h1 class="bpHeader">Our Investors</h1>
-        <p>Some text from the Investors page. Need ~85 words.</p>
+        <p>If you're curious about the world of Clean Burning Black Rocks Inc., be sure to check out our investors page. Here you can find various articles from across the world of Clean Burning Black Rocks from major scientific discoveries to our success in world politics! Of course, you can also see the amazing people that have decided, "Yes, I believe in Clean Burning Black Rocks and can tell that Clean Burning Black Rocks Inc. can lead us to a better future!" To check all this out, just click the link below!</p>
         <a href="investors.php" class="bpButton">More Information</a>
     </div>
 
     <div class="backgroundPanel homePageFlexBox">
         <h1 class="bpHeader">Invest</h1>
-        <p>Some text from the Invest page. Need ~85 words.</p>
+        <p>If you're an all-star believer in Clean Burning Black Rocks Inc. and get heated up thinking about our Clean Burning Black Rocks, you can click on over to the invest page. Here you can sign up to be an official investor to Clean Burning Black Rocks Inc. and choose if you want your righteous investment to be shown to all on our investors page! Any amount of investment will help us greatly in our future endeavors and general research. Just click the link below to get started!</p>
         <a href="#" class="bpButton">More Information</a>
     </div>
 </content>
